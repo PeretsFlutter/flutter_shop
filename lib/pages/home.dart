a5c40e8ae15fd7a43a9b337ba9aa15adb8cb0593
PeretsPasha <perets.pasha.pro@gmail.com>
2021-04-18 17:22:08 +0300

    Add Home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 85,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(35),
+                bottomRight: Radius.circular(35)),
+          ),
+          child: ListView(
+            padding: const EdgeInsets.all(10.0),
+            children: <Widget>[
+              Container(
+                child: ListTile(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
