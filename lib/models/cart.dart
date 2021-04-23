f615dec169a3c3c97dd2b09d24359b638d6fecca
PeretsPasha <perets.pasha.pro@gmail.com>
2021-04-23 17:23:30 +0300

    Add item

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -17,5 +17,21 @@ class Cart {
 }
 
 class CartDataProvider with ChangeNotifier {
+  Map<String, Cart> _cartItems = {};
 
+  UnmodifiableMapView<String, Cart> get cartItems =>
+      UnmodifiableMapView(_cartItems);
+
+  int get cartItemsCount => _cartItems.length;
+
+  double get totalAmount {
+    var total = 0.0;
+
+    _cartItems.forEach((key, item) {
+      total += item.number * item.price;
+    });
+    return total;
+  }
+
+  void addItem({productId, price, title, imgUrl}) {}
 }
