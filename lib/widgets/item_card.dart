ef174e721881299836d090a872624593ed2b3150
PeretsFlutter <ironmanbitcoinpro@gmail.com>
2021-05-03 19:52:36 +0300

    ItemCard

--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/product.dart';
 
 class ItemCard extends StatelessWidget {
+  const ItemCard({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    final product = Provider.of<Product>(context, listen: false);
+
+    return Container(
+      width: 150,
+      padding: const EdgeInsets.all(10.0),
+      margin: const EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        color: Color(int.parse(product.color)),
+      ),
+      child: Column(
+        children: [],
+      ),
+    );
   }
 }
